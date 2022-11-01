d8a73fda190d8d26ca02cfc52e9c2a0cd349d4f6
Glebovski <andrewglebovski@gmail.com>
2022-11-01 10:18:30 +0300

    Add prototypes

--- source/tree.hpp
+++ source/tree.hpp
@@ -18,3 +18,31 @@ typedef struct {
     Node *root = nullptr;               ///< Tree root node
     int size = 0;                       ///< Nodes count
 } Tree;
+
+
+/**
+ * \brief Constructs tree
+ * \param [out] tree Previously initialized tree structure
+ * \param [in]  root If not null, then it will be set as root of the tree,
+ * otherwise root will be allocated
+ * \return Non zero value means error
+*/
+int tree_constructor(Tree *tree, Node *root = nullptr);
+
+
+/**
+ * \brief Allocates new node
+ * \param [in] value New node's value
+ * \param [in] left  New node's left child
+ * \param [in] right New node's right child
+ * \return Pointer to node or nullptr if allocates fail
+*/
+Node *create_node(int value, Node *left = nullptr, Node *right = nullptr);
+
+
+/**
+ * \brief Destructs tree
+ * \param [in] tree All tree nodes will be free
+ * \return Non zero value means error
+*/
+int tree_destructor(Tree *tree);
