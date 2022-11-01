a9889ed5c8a263efa2f13a6ee64162028b7c2843
Glebovski <andrewglebovski@gmail.com>
2022-11-01 15:15:45 +0300

    Add write and read prototypes

---
+++ source/io.hpp
@@ -0,0 +1,16 @@
+/**
+ * \brief Reads tree from file
+ * \param [out] tree     Reads file in this tree
+ * \param [in]  filepath Path to input file
+ * \return Non zero value means error
+*/
+int read_tree(Tree *tree, const char *filepath);
+
+
+/**
+ * \brief Prints tree to file in specific format
+ * \param [out] tree     Prints this tree in file
+ * \param [in]  filepath Path to output file
+ * \return Non zero value means error
+*/
+int write_tree(Tree *, const char *filepath);
